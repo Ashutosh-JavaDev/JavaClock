97487377118103fd32a8b9a87a7f9f414bbbba49
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-04 10:51:36 +0530

    Clock

--- Project/src/Clock.java
+++ Project/src/Clock.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionListener;
 // import java.awt.GridLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.GridLayout;
 public class Clock extends JFrame implements ActionListener {
     JLabel[]label=new JLabel[12];
     Font font=new Font("Roboto",Font.BOLD,18);
@@ -21,12 +22,18 @@ public class Clock extends JFrame implements ActionListener {
         panel=new JPanel();
         panel.setBounds(10,10,480,480);
         panel.setBackground(Color.red);
+        panel.setLayout(new GridLayout(5,3,50,50));
         add(panel);
         for(int i=0;i<12;i++){
             label[i]=new JLabel(String.valueOf(i));
             label[i].setFont(font);
             label[i].setForeground(Color.white);
+            label[i].setFocusable(false);
         }
+        panel.add(label[11]);
+        panel.add(label[12]);
+        panel.add(label[1]);
+
     }
     public static void main(String[] args) throws Exception {
         new Clock();        
