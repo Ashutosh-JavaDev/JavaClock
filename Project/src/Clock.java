e964ae07ca8fd2129e34e2da81f99af542925cb7
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-04 10:56:07 +0530

    Clock

--- Project/src/Clock.java
+++ Project/src/Clock.java
@@ -24,29 +24,32 @@ public class Clock extends JFrame implements ActionListener {
         panel.setBackground(Color.red);
         panel.setLayout(new GridLayout(5,3,50,50));
         add(panel);
-        for(int i=0;i<12;i++){
-            label[i]=new JLabel(String.valueOf(i));
+     
+        for (int i = 0; i < 12; i++) {
+            label[i] = new JLabel(String.valueOf(i));
             label[i].setFont(font);
             label[i].setForeground(Color.white);
             label[i].setFocusable(false);
         }
-        panel.add(label[11]);
-        panel.add(label[12]);
-        panel.add(label[1]);
-        panel.add(label[10]);
-        // panel.add(label[]);
-        panel.add(label[2]);
-        panel.add(label[9]);
-        // panel.add(label[12]);
-        panel.add(label[3]);
-        panel.add(label[8]);
-        // panel.add(label[12]);
-        panel.add(label[4]);
-        panel.add(label[7]);
-        panel.add(label[6]);
-        panel.add(label[5]);
-        
 
+        // Adding labels to the panel in a specific order to represent a clock face
+        panel.add(label[11]);
+        panel.add(new JLabel("")); // Empty space
+        panel.add(label[0]);
+        panel.add(label[10]);
+        panel.add(new JLabel("")); // Empty space
+        panel.add(label[1]);
+        panel.add(label[9]);
+        panel.add(new JLabel("")); // Empty space
+        panel.add(label[2]);
+        panel.add(label[8]);
+        panel.add(label[7]);
+        panel.add(label[3]);
+        panel.add(label[6]);
+        panel.add(new JLabel("")); // Empty space
+        panel.add(label[4]);
+        panel.add(new JLabel("")); // Empty space
+        panel.add(label[5]);
     }
     public static void main(String[] args) throws Exception {
         new Clock();        
