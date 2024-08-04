ffa755f3e89bfc58281872b992dc73a43091126e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-04 10:41:54 +0530

    Clock

--- Project/src/Clock.java
+++ Project/src/Clock.java
@@ -3,6 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 import java.awt.GridLayout;
+import java.awt.Color;
 import java.awt.Font;
 public class Clock extends JFrame implements ActionListener {
     JPanel panel;
@@ -17,6 +18,9 @@ public class Clock extends JFrame implements ActionListener {
     }
     public void ClockFunction() {
         panel=new JPanel();
+        panel.setBounds(10,10,490,490);
+        panel.setBackground(Color.red);
+        add(panel);
     }
     public static void main(String[] args) throws Exception {
         new Clock();        
