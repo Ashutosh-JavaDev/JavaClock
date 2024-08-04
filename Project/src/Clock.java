1f9046cc1b9fe207c48c2a7ca64294ae8211d1dd
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-04 10:30:17 +0530

    Clock

--- Project/src/Clock.java
+++ Project/src/Clock.java
@@ -3,8 +3,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.GridLayout;
 import java.awt.Font;
-public class Clock {
+public class Clock extends JFrame implements ActionListener {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent ae){
+
     }
 }
